c31ab69fff8ce57dcf6e4aa54487c7feeb29dc04
CarlotaColunga <carlotacv23@educastur.es>
2025-10-22 11:04:10 +0200

    Add files via upload

---
+++ ColungaVilla_Carlota_Processing/sketch.js
@@ -0,0 +1,10 @@
+// La función setup() se ejecuta UNA sola vez al iniciar el programa
+function setup() {
+  createCanvas(400, 400); // crea un lienzo de 400x400 píxeles
+} alert("funciona")
+
+// La función draw() se ejecuta MUCHAS veces por segundo (bucle automático)
+function draw() {
+  background(70,5,230);        // pinta el fondo blanco en cada frame
+  // aquí irán las órdenes para dibujar tus figuras
+}
---
+++ ColungaVilla_Carlota_Processing/sketch1.js
@@ -0,0 +1,24 @@
+function setup() {
+  createCanvas(400, 400); 
+  background(255);        
+
+  
+  stroke(30, 255, 0);      
+  strokeWeight(6);        
+  line(100, 100, 300, 100);
+
+ 
+  stroke(180, 20, 160, 60); 
+  strokeWeight(20);        
+  line(300, 100, 300, 300);
+
+  
+  stroke(0, 0, 255);      
+  strokeWeight(18);        
+  line(300, 300, 100, 300);
+
+  
+  stroke(120,60,255);            
+  strokeWeight(25);       
+  line(100, 300, 100, 100);
+}
